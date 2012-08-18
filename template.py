427a21166b63ab4bfe4b2822ae6638c109b5b958
Jérôme Odier <jodier@odier-serveur.(none)>
2012-08-18 20:48:57 +0200

    update

--- template.py
+++ template.py
@@ -7,28 +7,13 @@ import zlib, base64
 #############################################################################
 
 configure_fp = open('configure.sh', "r")
-bussize_fp = open('bussize.c', "r")
-busorder_fp = open('busorder.c', "r")
 
 configure = configure_fp.read()
-bussize = bussize_fp.read().replace('%', '%%')
-busorder = busorder_fp.read().replace('%', '%%')
 
 #############################################################################
 
 if __name__ == '__main__':
 
-
-	#####################################################################
-	# TOOlS								    #
-	#####################################################################
-
-	TOOLS = 'cat > bussize_$$.c << EOF\n%s\nEOF\n\ncat > busorder_$$.c << EOF\n%s\nEOF' % (bussize, busorder)
-
-	configure = configure.replace('$$TOOLS$$', TOOLS)
-
-	#####################################################################
-	# TEMPLATE							    #
 	#####################################################################
 
 	configure = base64.b64encode(zlib.compress(configure, 9))
